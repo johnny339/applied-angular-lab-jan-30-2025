aa1b5e028e67f3ce079e6ee12214cfb8ffb6d66d
johnny339 <guzmanj774@gmail.com>
2025-01-30 13:25:28 -0500

    before starting lab

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,10 +1,23 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+
+import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>Welcome Home</p> `,
+
+  imports: [JsonPipe],
+
+  template: `
+    <p>Welcome Home</p>
+    <pre>{{ booksResource.value() | json }}</pre>
+  `,
+
   styles: ``,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  booksResource = resource({
+    loader: () => fetch('./api/books').then((b) => b.json()),
+  });
+}
